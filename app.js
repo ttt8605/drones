7638f2c01d786e20c518898b6ad9bfac6f9a3429
ttt8605 <theodorapostoaie@gmail.com>
2024-07-30 19:42:53 +0300

    gata2

--- app.js
+++ app.js
@@ -127,6 +127,9 @@ app.use((err, req, res, next) => {
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT,'0.0.0.0', () => {
-    console.log(`Server running at http://0.0.0.0:${PORT}/`);
+// app.listen(PORT,'0.0.0.0', () => {
+//     console.log(`Server running at http://0.0.0.0:${PORT}/`);
+// });
+app.listen(PORT, () => {
+    console.log(`Server running at ${PORT}/`);
 });
