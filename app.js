1f19346444ad6229746d702f126e781290f3e091
ttt8605 <64919222+ttt8605@users.noreply.github.com>
2024-08-02 12:17:04 +0300

    trying something

--- app.js
+++ app.js
@@ -73,8 +73,8 @@ const sessionConfig = {
     resave: false,
     saveUninitialized: true,
     cookie: {
-        httpOnly: true,
-        secure:true,
+        // httpOnly: true,
+        // secure:true,
         secure: process.env.NODE_ENV === 'production',
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
